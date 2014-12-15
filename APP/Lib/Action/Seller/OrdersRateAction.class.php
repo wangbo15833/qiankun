1814fa78afcbdf01b311bdbf492f397868d7d4b2
wangbo15833 <492948155@qq.com>
2014-12-15 17:20:13 +0800

    提交订单评价

---
+++ APP/Lib/Action/Seller/OrdersRateAction.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 14-12-15
+ * Time: 下午4:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+class OrdersRateAction extends CommonAction{
+    protected $tableName="OrdersRate";
+    public function add(){
+        $orderid=I('orderid');
+        $db=M('Orders');
+        $result=$db->where('orderid='.$orderid)->find();
+        $this->assign("orders",$result);
+        $this->display();
+    }
+
+    public function insert(){
+        $db=M($this->tableName);
+        $db->create();
+        $db->add_time=time();
+        $result=$db->add();
+        if($result){
+            $db=M("Orders");
+            $db->where("orderid=".$_POST['orderid'])->setField('shop_rate_id',$result);
+            $this->success("评价成功","__GROUP__/Orders/index");
+        }
+        else{
+            $this->error("评价失败");
+        }
+
+    }
+
+    public function index(){
+        echo "hello world";
+    }
+}
